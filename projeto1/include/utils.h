64d2ac1c6d1073a2bb6fb12bf3228e97cf085d8d
Gonçalo André Rodrigues Soares <62038187+gonaloppcc@users.noreply.github.com>
2022-10-18 19:53:40 +0100

    Update function names to english.

--- projeto1/include/utils.h
+++ projeto1/include/utils.h
@@ -1,10 +1,10 @@
 #ifndef UTILS
 #define UTILS
 
-void inicializa_vetor();
-void inicializa_clusters();
+void init_vector();
+void init_clusters();
 
-void atribui_clusters();
-void calcula_centroides();
+void assign_clusters();
+void compute_centroids();
 
 #endif
