eed87e48f4ea3b8563e5ff1501c043ccecef8454
Mapc1 <marcocosta579@gmail.com>
2022-10-18 16:21:29 +0100

    Add project structure

---
+++ projeto1/include/utils.h
@@ -0,0 +1,10 @@
+#ifndef UTILS
+#define UTILS
+
+void inicializa_vetor();
+void inicializa_clusters();
+
+void atribui_clusters();
+void calcula_centroides();
+
+#endif
---
+++ projeto1/src/k_means.c
@@ -0,0 +1,9 @@
+#include "../include/utils.h"
+
+int main(int argc, char const *argv[])
+{
+    float *points;
+    float *clusters;
+
+    return 0;
+}
