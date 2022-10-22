bf3a32a499539c4d18b65f0620116024bc7c051f
Gonçalo André Rodrigues Soares <62038187+gonaloppcc@users.noreply.github.com>
2022-10-23 00:58:47 +0100

    Fix bug in script.

--- projeto1/scripts/exec.sh
+++ projeto1/scripts/exec.sh
@@ -4,8 +4,7 @@ scp -r 'src' pg50393@s7edu2.di.uminho.pt:/home/pg50393/projetos/projeto1/
 
 scp -r 'Makefile' pg50393@s7edu2.di.uminho.pt:/home/pg50393/projetos/projeto1
 
-ssh pg50393@s7edu2.di.uminho.pt \
-"cd /home/pg50393/projetos/projeto1/;
+ssh pg50393@s7edu2.di.uminho.pt "cd /home/pg50393/projetos/projeto1/;
 module load gcc/7.2.0;
 make clean;
 make;
