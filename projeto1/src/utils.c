c0a716831fd70065c8e7046ff4971c7fd7e0a2d7
Gonçalo André Rodrigues Soares <62038187+gonaloppcc@users.noreply.github.com>
2022-10-27 18:13:56 +0100

    Add logs files and remove unneeded sqrt.

--- projeto1/src/utils.c
+++ projeto1/src/utils.c
@@ -48,7 +48,7 @@ PArray init_samples(int n) {
 }
 
 inline static float euclidean_distance(Cluster cluster, Point point) {
-    return sqrtf(powf(cluster->x - point->x, 2) + (powf(cluster->y - point->y, 2)));
+    return powf(cluster->x - point->x, 2) + (powf(cluster->y - point->y, 2));
 }
 
 /**
