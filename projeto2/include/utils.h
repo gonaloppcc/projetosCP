6d0e4fb9e87026eae68dc1ce29ca7e06b6573e0b
Gonçalo André Rodrigues Soares <62038187+gonaloppcc@users.noreply.github.com>
2022-11-09 01:36:48 +0000

    Setup project2.

---
+++ projeto2/include/utils.h
@@ -0,0 +1,37 @@
+#ifndef UTILS
+#define UTILS
+
+/* ------------------------------------------------ Type Definitions ------------------------------------------------ */
+
+struct point {
+    float x;
+    float y;
+    int cluster;
+};
+
+struct cluster {
+    float x;
+    float y;
+
+    int samples_size; // Number of samples associated with this cluster
+};
+
+typedef struct point Point;
+
+typedef Point *PArray;
+
+typedef struct cluster Cluster;
+
+typedef Cluster *CArray;
+
+/* --------------------------------------------- Functions declarations --------------------------------------------- */
+
+PArray init_samples(int n);
+
+CArray init_clusters(CArray clusters, PArray samples, int k);
+
+int assign_clusters(PArray samples, int n, CArray clusters, int k);
+
+void compute_centroids(PArray samples, int n, CArray clusters, int k);
+
+#endif
